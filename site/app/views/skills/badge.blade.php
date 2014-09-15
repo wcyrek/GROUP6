d7362caacac28ac27fbacc072df1825f21ca4c8f
wcyrek <github@witoldcyrek.com>
2014-09-15 19:16:20 -0400

    Adding badge view

---
+++ site/app/views/skills/badge.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('title')
+	<title>Skills List</title>
+@endsection
+
+@section('content')
+	<div id="badges">
+	@foreach($skills as $skill)
+		<div id="{{ $skill->id }}" class="noselect badge skill_{{ $skill->id }}">
+			<label class="badge_caption">{{ $skill->name }}</label>
+		</div>
+	@endforeach
+	</div>
+@endsection
+
