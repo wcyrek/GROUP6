efd72200a305bc20de73d54f4782fe7c0f86900b
apisama <apimesa@gmail.com>
2014-09-15 19:26:33 -0400

    login routes now to a post form

--- site/app/routes.php
+++ site/app/routes.php
@@ -34,4 +34,4 @@ Route::get('skilltypes/{id}', array('as'=>'skilltypes', 'uses'=>'SkillTypesContr
 //Admin
 Route::get('admin', array('as'=>'admin', 'uses'=>'AdminController@getIndex'));
 
-Route::get('login', 'LoginController@login');
+Route::get('login', 'LoginController@postLogin');
