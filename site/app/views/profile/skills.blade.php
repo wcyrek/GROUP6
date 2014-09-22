ee8d48396e1bee1697e0c51ab0a719995e8229bd
apisama <apimesa@gmail.com>
2014-09-22 16:00:16 -0400

    added drag and drop for deletion

--- site/app/views/profile/skills.blade.php
+++ site/app/views/profile/skills.blade.php
@@ -21,10 +21,17 @@
 {{ Form::close() }}	
 	
 <h2> Your skills: </h2>
+<div id="badges">
+	@foreach($profile->skills as $skill)
+		<div id="{{ $skill->id }}" class="noselect badge skill_{{ $skill->id%5+1 }}" draggable="true" ondragstart="drag(event)">
+			<label class="badge_caption">{{ $skill->name }}</label>
+		</div>
+	@endforeach
+</div>
 <ul>
 @foreach($profile->skills as $skill)
 	<li> 
-	{{ Form::open(array('action' => array('ProfileController@deleteSkill', $profile->id ))) }}
+	{{ Form::open(array('action' => array('ProfileController@deleteSkill', $profile->id ), 'draggable'=> 'true', 'id' => 'form'.$skill->id, 'style' => 'display:none' )) }}
 	{{ Form::hidden('skill', $skill->id) }}
 	{{ Form::label($skill->name)  }} 
 	<i>
@@ -36,7 +43,23 @@
 </ul>
 
 {{ HTML::linkAction('ProfileController@getProfile', 'back to profile page', array('id' => $profile->id)) }}
+<div class="icon-trash" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+<script>
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+function drag(ev) {
+	ev.dataTransfer.setData("text/html", ev.target.id);
+}
 
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text/html");
+    document.getElementById("form" + data).submit();
+    ev.target.appendChild(document.getElementById(data));
+}
+
+</script>
 @endsection
 
 
