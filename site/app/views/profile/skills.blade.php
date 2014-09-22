2bf11c96c936d3693d6229e727e60ebdd7ad8849
apisama <apimesa@gmail.com>
2014-09-22 17:12:57 -0400

    added drag and drop to add skills

--- site/app/views/profile/skills.blade.php
+++ site/app/views/profile/skills.blade.php
@@ -9,7 +9,8 @@
 
 @section('content')
 
-{{ Form::open(array('action' => array('ProfileController@postEditSkills', $profile->id ), 'class' => 'skills-form')) }}
+{{ Form::open(array('action' => array('ProfileController@postEditSkills', $profile->id ),
+	 'class' => 'skills-form', 'id'=>'form_select')) }}
 
 
 <br>
@@ -20,14 +21,27 @@
 
 {{ Form::close() }}	
 	
-<h2> Your skills: </h2>
-<div id="badges">
-	@foreach($profile->skills as $skill)
-		<div id="{{ $skill->id }}" class="noselect badge skill_{{ $skill->id%5+1 }}" draggable="true" ondragstart="drag(event)">
-			<label class="badge_caption">{{ $skill->name }}</label>
-		</div>
-	@endforeach
+<h2> Add skills: </h2>
+<div id="skills-select" class="skill_overflow" style="float:left" ondrop="dropAdd(event)" ondragover="allowDrop(event)" >
+	<div id="badges">
+		@foreach($profile->skills as $skill)
+			<div id="{{ $skill->id }}" class="drag_badge noselect badge skill_{{ $skill->id%5+1 }}" draggable="true" ondragstart="drag(event)">
+				<label class="badge_caption">{{ $skill->name }}</label>
+			</div><br>
+		@endforeach
+	</div>
 </div>
+<div class="skill_overflow" style="float:right">
+	<div id="badges">
+	
+		@foreach($skills as $key=>$val)
+			<div id="{{ $key }}" class="drag_badge noselect badge skill_{{ $key%5+1 }}" draggable="true" ondragstart="drag(event)">
+				<label class="badge_caption">{{ $val}}</label>
+			</div><br>			
+		@endforeach
+	</div>
+</div>
+<div style="clear:both"></div>
 <ul>
 @foreach($profile->skills as $skill)
 	<li> 
@@ -56,9 +70,18 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text/html");
     document.getElementById("form" + data).submit();
-    ev.target.appendChild(document.getElementById(data));
+    
 }
 
+function dropAdd(ev){
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text/html");
+	var form = document.getElementById("form_select");
+	form.type_id.value = data;
+	form.submit();
+	
+	
+}
 </script>
 @endsection
 
