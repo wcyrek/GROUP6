f4539a3260441eb815051df4b97f01fad014b1a8
wcyrek <github@witoldcyrek.com>
2014-09-22 00:35:03 -0400

    Broken route

--- site/app/views/menu.blade.php
+++ site/app/views/menu.blade.php
@@ -1,3 +1,11 @@
 <ul id="menu">
-	<li>{{ HTML::linkRoute('home', 'Home') }}</li><li>test2</li>
+	<li class="first"/><li>{{ HTML::linkRoute('home', 'Home') }}</li><!-- 
+@if (!Auth::check())
+	
+@elseif(Auth::user()->admin)
+	--><li>{{ HTML::linkRoute('skills', 'Skills') }}</li><li>{{ HTML::linkRoute('skilltypes', 'Skilltypes') }}</li><li>{{ HTML::linkRoute('users', 'Users') }}</li><!--  -->
+@else
+	
+@endif
+	--><li>{{ HTML::linkRoute('about', 'About') }}</li><li class="last"/>
 </ul>
